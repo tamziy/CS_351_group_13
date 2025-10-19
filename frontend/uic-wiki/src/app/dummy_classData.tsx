48585017a5bc273ad6f82e918c177819f5d3ebca
tdcoleman127 <figgeryfirehead@gmail.com>
2025-10-18 23:13:59 -0500

    Added file with possible dummy data for classes

---
+++ frontend/uic-wiki/src/app/dummy_classData.tsx
@@ -0,0 +1,29 @@
+export const classData = [
+  /* Initial thesis of: What could possibly be JSON-ified regarding class content
+  Some of these things can be "mapped" out from arrays/lists of content
+  Course Number
+  Class Title
+  Class Summary
+  Professors Names
+  Credit Hours
+  Career Tracks
+  Prerequisite Classes
+  Resources
+  Rating
+
+  *all subject to change during backend development
+  */
+
+  {
+    courseNo: 251,
+    classTitle: "Data Structures",
+    classSummary: "",
+    professors: ["Ayala, Koehler, Theys, Bello, Katok, Troy, Reed, Buy, Grechanik"],
+    credit_hours: "",
+    career_tracks: [],
+    prereqs: [],
+    resources: [],
+    rating: "4",
+  },
+  
+];
