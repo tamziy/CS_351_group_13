2b0c45bef5688b92c8a1784f07602055b4ac010f
tamziy <114886626+tamziy@users.noreply.github.com>
2025-10-21 18:09:17 -0500

    Add Sidebar generic component with navigation links and UIC logo; added  lucide-react PLEASE NPM INSTALL

--- frontend/uic-wiki/src/app/layout.tsx
+++ frontend/uic-wiki/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/Slidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex min-h-screen`}
       >
-        {children}
+        <Sidebar />
+        <main className="flex-1 p-6 bg-gray-50">
+          {children}
+        </main>
       </body>
     </html>
   );
