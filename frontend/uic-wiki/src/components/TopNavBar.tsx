013dac2e2fdeb091f74bfb4052808d44cbce09c0
Rohan Jawale <jawalerohan2005@gmail.com>
2025-10-23 07:18:59 -0500

    Added functionality to search bar autosuggest feature

--- frontend/uic-wiki/src/components/TopNavBar.tsx
+++ frontend/uic-wiki/src/components/TopNavBar.tsx
@@ -1,8 +1,29 @@
 "use client";
 
+import { useState } from "react";
 import { Search, MessageCircle, Bell, Settings } from "lucide-react";
+import Link from "next/link";
+
 
 export default function Navbar() {
+
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value;
+    setValue(val);
+
+    if (val.length > 0){
+      const res = await fetch(`http://127.0.0.1:5000/suggest?q=${encodeURIComponent(val)}`);
+      const data = await res.json();
+      setSuggestions(data.suggestions)
+    }
+    else{
+      setSuggestions([]);
+    }
+  }
+
   return (
     <header className="relative flex items-center justify-between w-full h-16 bg-white border-b px-6">
       {/* Centered Search Bar */}
@@ -12,8 +33,32 @@ export default function Navbar() {
           <input
             type="text"
             placeholder="Search"
+            value={value}
+            onChange={onChange}
             className="bg-transparent outline-none text-sm text-black w-full"
           />
+          {suggestions.length > 0 && (
+            <ul className="absolute top-10 left-0 bg-white border border-gray-200 rounded-md shadow w-full z-10">
+              {suggestions.map((item) => {
+                const classNumber = item.split(" ")[1];
+                return (
+                  <li key={item}>
+                    <Link
+                      href={`/classes/${classNumber}`}
+                      className="block p-2 hover:bg-gray-100 cursor-pointer"
+                      onClick={()=> {
+                        setValue("")
+                        setSuggestions([])
+                      }}
+                    >
+                      {item}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+
         </div>
       </div>
 
