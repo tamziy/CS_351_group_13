c1fbb2486d30a2e7efa6512d8b912c4a3e2b0bb8
tdcoleman127 <figgeryfirehead@gmail.com>
2025-11-17 20:55:20 -0600

    Beginning major layout edits to class page

--- frontend/uic-wiki/src/app/classPage/classpage_all.tsx
+++ frontend/uic-wiki/src/app/classPage/classpage_all.tsx
@@ -28,6 +28,26 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion"
 
+import { TrendingUp } from "lucide-react"
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis, YAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "A bar chart with a custom label"
+
 
 
 interface ItemProps {
@@ -170,6 +190,98 @@ const ClassPage: React.FC<MyComponentProps> = ({ item }) => {
     )
   }
 
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-2)",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "var(--chart-2)",
+  },
+  label: {
+    color: "var(--background)",
+  },
+} satisfies ChartConfig
+
+ function ChartBarLabelCustom() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Bar Chart - Custom Label</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            layout="vertical"
+            margin={{
+              right: 16,
+            }}
+          >
+            <CartesianGrid horizontal={false} />
+            <YAxis
+              dataKey="month"
+              type="category"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+              hide
+            />
+            <XAxis dataKey="desktop" type="number" hide />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="line" />}
+            />
+            <Bar
+              dataKey="desktop"
+              layout="vertical"
+              fill="var(--color-desktop)"
+              radius={4}
+            >
+              <LabelList
+                dataKey="month"
+                position="insideLeft"
+                offset={8}
+                className="fill-(--color-label)"
+                fontSize={12}
+              />
+              <LabelList
+                dataKey="desktop"
+                position="right"
+                offset={8}
+                className="fill-foreground"
+                fontSize={12}
+              />
+            </Bar>
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 leading-none font-medium">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="text-muted-foreground leading-none">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
+
+
   function ClassLevel({ title, codes, color }: { title: string; codes: string[]; color: string }) {
     return (
       <div>
@@ -194,28 +306,27 @@ const ClassPage: React.FC<MyComponentProps> = ({ item }) => {
 
   return (
     <main className="bg-gray-50 items-center min-h-full text-black">
-      <div className="grid grid-cols-[50%_20%] gap-6 justify-center">
-        <div className="p-4 rounded-2xl space-y-4">
+      <div className="grid grid-cols-[70%_20%] gap-6 justify-center">
+        <div className="p-4 rounded-2xl space-y-4 text-wrap">
           <div className="flex flex-row gap-4">
             <ClassIcon />
             <h1 className="text-3xl"><b>{item.classTitle}</b></h1>
           </div>
           <br></br>
-          <div className="bg-[#E0E0E0] p-4 rounded-3xl">
-            <h2 className="text-2xl"><u>Summary:</u></h2>
+          <div className="bg-[#E0E0E0] p-4 rounded-3xl text-wrap">
+            <h2 className="text-2xl">Summary:</h2>
             <p className="text-lg p-4">{item.classSummary}</p>
           </div>
-          <div className="bg-[#E0E0E0] p-4 rounded-3xl">
+          <div className="bg-[#E0E0E0] p-4 rounded-3xl text-wrap">
             <h2 className="text-2xl"><em>CS students say:</em></h2>
             <p className="text-lg p-4">&quot;{item.reviews}&quot;</p>
           </div>
         </div>
-        <div>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
+        <div className="mt-24">
           <AccordionDemo />
+        </div>
+        <div className="mt-24">
+          <ChartBarLabelCustom />  
         </div>
       </div>
 
@@ -224,3 +335,29 @@ const ClassPage: React.FC<MyComponentProps> = ({ item }) => {
 }
 
 export default ClassPage;
+
+
+<main className="bg-gray-50 items-center min-h-full text-black">
+      <div className="grid grid-cols-[70%_20%] gap-6 justify-center">
+        <div className="p-4 rounded-2xl space-y-4 text-wrap">
+          <div className="flex flex-row gap-4">
+            <Icon />
+            <h1 className="text-3xl"><b>{item.Title}</b></h1>
+          </div>
+          <br></br>
+          <div className="bg-[#E0E0E0] p-4 rounded-3xl text-wrap">
+            <h2 className="text-2xl">Summary:</h2>
+            <p className="text-lg p-4">{item.Summary}</p>
+          </div>
+          <div className="bg-[#E0E0E0] p-4 rounded-3xl text-wrap">
+            <h2 className="text-2xl"><em>Review:</em></h2>
+            <p className="text-lg p-4">&quot;{item.reviews}&quot;</p>
+          </div>
+        </div>
+        <div className="mt-24">
+          <Accordion />
+        </div>
+        <div className="mt-24">
+          <ChartBar />  
+        </div>
+      </div>
